3abfd62aaadf474213b7428b845409224f27b4f3
Appianin Obiri-Yeboah <appianinobiri@gmail.com>
2020-05-29 00:09:25 +1000

    add drop down hover functionality

--- inc/customizer.php
+++ inc/customizer.php
@@ -10,37 +10,40 @@
  *
  * @param WP_Customize_Manager $wp_customize Theme Customizer object.
  */
-function jazz_club_customize_register( $wp_customize ) {
-	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
-	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
-	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+function jazz_club_customize_register($wp_customize)
+{
+    $wp_customize->get_setting('blogname')->transport = 'postMessage';
+    $wp_customize->get_setting('blogdescription')->transport = 'postMessage';
+    $wp_customize->get_setting('header_textcolor')->transport = 'postMessage';
 
-	if ( isset( $wp_customize->selective_refresh ) ) {
-		$wp_customize->selective_refresh->add_partial(
-			'blogname',
-			array(
-				'selector'        => '.site-title a',
-				'render_callback' => 'jazz_club_customize_partial_blogname',
-			)
-		);
-		$wp_customize->selective_refresh->add_partial(
-			'blogdescription',
-			array(
-				'selector'        => '.site-description',
-				'render_callback' => 'jazz_club_customize_partial_blogdescription',
-			)
-		);
-	}
+    if (isset($wp_customize->selective_refresh)) {
+        $wp_customize->selective_refresh->add_partial(
+            'blogname',
+            array(
+                'selector' => '.site-title a',
+                'render_callback' => 'jazz_club_customize_partial_blogname',
+            )
+        );
+        $wp_customize->selective_refresh->add_partial(
+            'blogdescription',
+            array(
+                'selector' => '.site-description',
+                'render_callback' => 'jazz_club_customize_partial_blogdescription',
+            )
+        );
+    }
 }
-add_action( 'customize_register', 'jazz_club_customize_register' );
+
+add_action('customize_register', 'jazz_club_customize_register');
 
 /**
  * Render the site title for the selective refresh partial.
  *
  * @return void
  */
-function jazz_club_customize_partial_blogname() {
-	bloginfo( 'name' );
+function jazz_club_customize_partial_blogname()
+{
+    bloginfo('name');
 }
 
 /**
@@ -48,14 +51,17 @@ function jazz_club_customize_partial_blogname() {
  *
  * @return void
  */
-function jazz_club_customize_partial_blogdescription() {
-	bloginfo( 'description' );
+function jazz_club_customize_partial_blogdescription()
+{
+    bloginfo('description');
 }
 
 /**
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
-function jazz_club_customize_preview_js() {
-	wp_enqueue_script( 'jazz-club-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+function jazz_club_customize_preview_js()
+{
+    wp_enqueue_script('jazz-club-customizer', get_template_directory_uri() . 'dist/js/customizer.js', array('customize-preview'), '20151215', true);
 }
-add_action( 'customize_preview_init', 'jazz_club_customize_preview_js' );
+
+add_action('customize_preview_init', 'jazz_club_customize_preview_js');
