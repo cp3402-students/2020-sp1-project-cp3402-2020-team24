77b8b4b907d631ed28eeed274dcc6151430d6283
Appianin Obiri-Yeboah <appianinobiri@gmail.com>
2020-05-28 23:15:38 +1000

    add style editing css add bootstrap install

--- functions.php
+++ functions.php
@@ -7,102 +7,110 @@
  * @package jazz-club
  */
 
-if ( ! defined( '_S_VERSION' ) ) {
-	// Replace the version number of the theme on each release.
-	define( '_S_VERSION', '1.0.0' );
+if (!defined('_S_VERSION')) {
+    // Replace the version number of the theme on each release.
+    define('_S_VERSION', '1.0.0');
 }
 
-if ( ! function_exists( 'jazz_club_setup' ) ) :
-	/**
-	 * Sets up theme defaults and registers support for various WordPress features.
-	 *
-	 * Note that this function is hooked into the after_setup_theme hook, which
-	 * runs before the init hook. The init hook is too late for some features, such
-	 * as indicating support for post thumbnails.
-	 */
-	function jazz_club_setup() {
-		/*
-		 * Make theme available for translation.
-		 * Translations can be filed in the /languages/ directory.
-		 * If you're building a theme based on jazz-club, use a find and replace
-		 * to change 'jazz-club' to the name of your theme in all the template files.
-		 */
-		load_theme_textdomain( 'jazz-club', get_template_directory() . '/languages' );
+if (!function_exists('jazz_club_setup')) :
+    /**
+     * Sets up theme defaults and registers support for various WordPress features.
+     *
+     * Note that this function is hooked into the after_setup_theme hook, which
+     * runs before the init hook. The init hook is too late for some features, such
+     * as indicating support for post thumbnails.
+     */
+    function jazz_club_setup()
+    {
+        /*
+         * Make theme available for translation.
+         * Translations can be filed in the /languages/ directory.
+         * If you're building a theme based on jazz-club, use a find and replace
+         * to change 'jazz-club' to the name of your theme in all the template files.
+         */
+        load_theme_textdomain('jazz-club', get_template_directory() . '/languages');
 
-		// Add default posts and comments RSS feed links to head.
-		add_theme_support( 'automatic-feed-links' );
+        // Add default posts and comments RSS feed links to head.
+        add_theme_support('automatic-feed-links');
 
-		/*
-		 * Let WordPress manage the document title.
-		 * By adding theme support, we declare that this theme does not use a
-		 * hard-coded <title> tag in the document head, and expect WordPress to
-		 * provide it for us.
-		 */
-		add_theme_support( 'title-tag' );
+        /*
+         * Let WordPress manage the document title.
+         * By adding theme support, we declare that this theme does not use a
+         * hard-coded <title> tag in the document head, and expect WordPress to
+         * provide it for us.
+         */
+        add_theme_support('title-tag');
 
-		/*
-		 * Enable support for Post Thumbnails on posts and pages.
-		 *
-		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-		 */
-		add_theme_support( 'post-thumbnails' );
+        /*
+         * Enable support for Post Thumbnails on posts and pages.
+         *
+         * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+         */
+        add_theme_support('post-thumbnails');
 
-		// This theme uses wp_nav_menu() in one location.
-		register_nav_menus(
-			array(
-				'menu-1' => esc_html__( 'Primary', 'jazz-club' ),
-			)
-		);
+        // This theme uses wp_nav_menu() in one location.
+        register_nav_menus(
+            array(
+                'primary' => esc_html__('Primary', 'jazz-club'),
+            )
+        );
 
-		/*
-		 * Switch default core markup for search form, comment form, and comments
-		 * to output valid HTML5.
-		 */
-		add_theme_support(
-			'html5',
-			array(
-				'search-form',
-				'comment-form',
-				'comment-list',
-				'gallery',
-				'caption',
-				'style',
-				'script',
-			)
-		);
+        /*
+         * Switch default core markup for search form, comment form, and comments
+         * to output valid HTML5.
+         */
+        add_theme_support(
+            'html5',
+            array(
+                'search-form',
+                'comment-form',
+                'comment-list',
+                'gallery',
+                'caption',
+                'style',
+                'script',
+            )
+        );
 
-		// Set up the WordPress core custom background feature.
-		add_theme_support(
-			'custom-background',
-			apply_filters(
-				'jazz_club_custom_background_args',
-				array(
-					'default-color' => 'ffffff',
-					'default-image' => '',
-				)
-			)
-		);
+        // Set up the WordPress core custom background feature.
+        add_theme_support(
+            'custom-background',
+            apply_filters(
+                'jazz_club_custom_background_args',
+                array(
+                    'default-color' => 'ffffff',
+                    'default-image' => '',
+                )
+            )
+        );
 
-		// Add theme support for selective refresh for widgets.
-		add_theme_support( 'customize-selective-refresh-widgets' );
+        // Add theme support for selective refresh for widgets.
+        add_theme_support('customize-selective-refresh-widgets');
 
-		/**
-		 * Add support for core custom logo.
-		 *
-		 * @link https://codex.wordpress.org/Theme_Logo
-		 */
-		add_theme_support(
-			'custom-logo',
-			array(
-				'height'      => 250,
-				'width'       => 250,
-				'flex-width'  => true,
-				'flex-height' => true,
-			)
-		);
-	}
+        /**
+         * Add support for core custom logo.
+         *
+         * @link https://codex.wordpress.org/Theme_Logo
+         */
+        add_theme_support(
+            'custom-logo',
+            array(
+                'height' => 250,
+                'width' => 250,
+                'flex-width' => true,
+                'flex-height' => true,
+            )
+        );
+    }
 endif;
-add_action( 'after_setup_theme', 'jazz_club_setup' );
+add_action('after_setup_theme', 'jazz_club_setup');
+
+function jazz_club_add_editor_style()
+{
+    add_editor_style('dist/css/editor-style.css');
+}
+
+add_action('admin_init', 'jazz_club_editor_style');
 
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
@@ -111,50 +119,42 @@ add_action( 'after_setup_theme', 'jazz_club_setup' );
  *
  * @global int $content_width
  */
-function jazz_club_content_width() {
-	// This variable is intended to be overruled from themes.
-	// Open WPCS issue: {@link https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards/issues/1043}.
-	// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
-	$GLOBALS['content_width'] = apply_filters( 'jazz_club_content_width', 640 );
+function jazz_club_content_width()
+{
+    // This variable is intended to be overruled from themes.
+    // Open WPCS issue: {@link https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards/issues/1043}.
+    // phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
+    $GLOBALS['content_width'] = apply_filters('jazz_club_content_width', 1140);
 }
-add_action( 'after_setup_theme', 'jazz_club_content_width', 0 );
 
-/**
- * Register widget area.
- *
- * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
- */
-function jazz_club_widgets_init() {
-	register_sidebar(
-		array(
-			'name'          => esc_html__( 'Sidebar', 'jazz-club' ),
-			'id'            => 'sidebar-1',
-			'description'   => esc_html__( 'Add widgets here.', 'jazz-club' ),
-			'before_widget' => '<section id="%1$s" class="widget %2$s">',
-			'after_widget'  => '</section>',
-			'before_title'  => '<h2 class="widget-title">',
-			'after_title'   => '</h2>',
-		)
-	);
-}
-add_action( 'widgets_init', 'jazz_club_widgets_init' );
+add_action('after_setup_theme', 'jazz_club_content_width', 0);
+
 
 /**
  * Enqueue scripts and styles.
  */
-function jazz_club_scripts() {
-	wp_enqueue_style( 'jazz-club-style', get_stylesheet_uri(), array(), _S_VERSION );
-	wp_style_add_data( 'jazz-club-style', 'rtl', 'replace' );
+function jazz_club_scripts()
+{
+    wp_enqueue_style('jazz-club-bs-css', get_template_directory_url() . '/dist/css/bootstrap.min.css');
+    wp_enqueue_style('jazz-club-fontawesome', get_template_directory_url() . '/fonts/font-awesome/css/font-awesome.min.css');
+    wp_enqueue_style('jazz-club-style', get_stylesheet_uri(), array(), _S_VERSION);
+    wp_register_script('popper', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.4.0/cjs/popper.min.js', false, '', true);
+    wp_enqueue_script('popper');
+    wp_enqueue_script('jazz-club-tether' , get_template_directory_url() .'/src/js/tether.min.js', array(), '20170115', true);
+    wp_enqueue_script('jazz-club-bootstrap' , get_template_directory_url() .'/src/js/bootstrap.min.js', array('jquery'), '20170115', true);
+    wp_enqueue_script('jazz-club-bootstrap-hover' , get_template_directory_url() .'/src/js/bootstrap-hover.js', array('jquery'), '20170115', true);
+    wp_enqueue_script('jazz-club-nav-scroll' , get_template_directory_url() .'/src/js/nav-scroll.js', array('jquery'), '20170115', true);
+    wp_style_add_data('jazz-club-style', 'rtl', 'replace');
 
-	wp_enqueue_script( 'jazz-club-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
 
-	wp_enqueue_script( 'jazz-club-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), _S_VERSION, true );
+    wp_enqueue_script('jazz-club-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), _S_VERSION, true);
 
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
+    if (is_singular() && comments_open() && get_option('thread_comments')) {
+        wp_enqueue_script('comment-reply');
+    }
 }
-add_action( 'wp_enqueue_scripts', 'jazz_club_scripts' );
+
+add_action('wp_enqueue_scripts', 'jazz_club_scripts');
 
 /**
  * Implement the Custom Header feature.
@@ -179,7 +179,7 @@ require get_template_directory() . '/inc/customizer.php';
 /**
  * Load Jetpack compatibility file.
  */
-if ( defined( 'JETPACK__VERSION' ) ) {
-	require get_template_directory() . '/inc/jetpack.php';
+if (defined('JETPACK__VERSION')) {
+    require get_template_directory() . '/inc/jetpack.php';
 }
 
