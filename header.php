eecd0e96f33aeb6b2fe005a3d4bf07b9e2377ad8
Appianin Obiri-Yeboah <appianinobiri@gmail.com>
2020-05-29 01:05:09 +1000

    fix navbar not showing up

--- header.php
+++ header.php
@@ -13,47 +13,62 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="profile" href="https://gmpg.org/xfn/11">
 
-	<?php wp_head(); ?>
+    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'jazz-club' ); ?></a>
+<div id="page" class="site container">
+    <a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e('Skip to content', 'jazz-club'); ?></a>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$jazz_club_description = get_bloginfo( 'description', 'display' );
-			if ( $jazz_club_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $jazz_club_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+    <nav id="masthead" class="site-header">
+        <nav id="menu" class="navbar navbar-expand-md navbar-light" role="navigation"></nav>
+        <div class="site-branding navbar-brand">
+            <?php
+            the_custom_logo();
+            if (is_front_page() && is_home()) :
+                ?>
+                <h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>"
+                                          rel="home"><?php bloginfo('name'); ?></a></h1>
+            <?php
+            else :
+                ?>
+                <p class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>"
+                                         rel="home"><?php bloginfo('name'); ?></a></p>
+            <?php
+            endif;
+            $jazz_club_description = get_bloginfo('description', 'display');
+            if ($jazz_club_description || is_customize_preview()) :
+                ?>
+                <p class="site-description"><?php echo $jazz_club_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+                    ?></p>
+            <?php endif; ?>
+        </div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'jazz-club' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+                data-target="#bs4navbar" aria-controls="bs4navbar" aria-expanded="false"
+                aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <?php
+        wp_nav_menu([
+            'menu' => 'primary',
+            'theme_location' => 'primary',
+            'container' => 'div',
+            'container_id' => 'bs4navbar',
+            'container_class' => 'collpase navbar-collapse',
+            'menu_id' => 'main-menu',
+            'menu_class' => 'navbar-nav ml-auto',
+            'depth' => 2,
+            'fallback_cb' => 'bs4navwalker::fallback',
+            'walker' => new bs4navwalker()
+        ]);
+        ?>
+    </nav>
+
+
+    </div><!-- #masthead -->
